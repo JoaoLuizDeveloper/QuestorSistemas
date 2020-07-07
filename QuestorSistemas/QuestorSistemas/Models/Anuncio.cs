c9962037dfe830d929f0510699e05189f1dda0f3
JoaoLuizDeveloper <juninhoworld2012@gmail.com>
2020-07-07 18:56:54 -0300

    firstcode

---
+++ QuestorSistemas/QuestorSistemas/Models/Anuncio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestorSistemas.Models
+{
+    public class Anuncio
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string ValorCompra { get; set; }
+        public string ValorVenda { get; set; }
+        public string Cor { get; set; }
+        public string Combustível { get; set; }
+        public DateTime DataVenda { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ QuestorSistemas/QuestorSistemas/Models/contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestorSistemas.Models
+{
+    public class contexto : DbContext
+    {
+        public DbSet<Marca> Marca { get; set; }
+        public DbSet<Modelo> Modelo { get; set; }
+        public DbSet<Anuncio> Anuncio { get; set; }
+    }
+}
